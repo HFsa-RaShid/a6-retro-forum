22a31fc9df4792d4e20c6919a0be7425ec8c9123
HFsa-RaShid <hafsa.cse7.bu@gmail.com>
2024-03-02 23:40:55 +0600

    nav Added

---
+++ retro.js
@@ -0,0 +1,31 @@
+function showToolTip1() {
+    document.getElementById("toolTip1").classList.remove("hidden");
+  }
+  
+  function hideToolTip1() {
+    document.getElementById("toolTip1").classList.add("hidden");
+  }
+
+  function showToolTip2() {
+    document.getElementById("toolTip2").classList.remove("hidden");
+  }
+  
+  function hideToolTip2() {
+    document.getElementById("toolTip2").classList.add("hidden");
+  }
+
+  function showToolTip3() {
+    document.getElementById("toolTip3").classList.remove("hidden");
+  }
+  
+  function hideToolTip3() {
+    document.getElementById("toolTip3").classList.add("hidden");
+  }
+
+  function showToolTip4() {
+    document.getElementById("toolTip4").classList.remove("hidden");
+  }
+  
+  function hideToolTip4() {
+    document.getElementById("toolTip4").classList.add("hidden");
+  }
