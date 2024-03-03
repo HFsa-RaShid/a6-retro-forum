1f7c8a4b6aa813b421e9226db1dc2fef2d6c894b
HFsa-RaShid <hafsa.cse7.bu@gmail.com>
2024-03-03 14:55:53 +0600

    appened div Added

--- retro.js
+++ retro.js
@@ -43,9 +43,13 @@ const loadPost= async () =>{
 }
 const displayPosts = post =>{
     const postContainer=document.getElementById('post-container');
+    const postReadMark=document.getElementById('post-readMark');
     post.forEach(postInfo => {
+        const active = postInfo.isActive;
+
         const postCard= document.createElement(`div`);
-        postCard.classList= `w-full bg-grey flex`
+        
+        postCard.classList.add= `w-full bg-grey flex`;
         postCard.innerHTML= `
         <div class="flex h-[200px] bg-[#F3F3F5] w-full mb-10 rounded-2xl pt-10" >
                 <div class="w-[20%] ">
@@ -53,12 +57,10 @@ const displayPosts = post =>{
                         <img src="${postInfo.image}" alt="" srcset="" class="rounded-xl">
                         
                         <div class="bg-white  rounded-full absolute w-[14px] h-[14px] top-0 right-0 ">
-                            <div class="bg-green-700  rounded-full absolute w-3 h-3 top-0 right-0 "></div>
+
+                            <div id="activeColor" class="rounded-full absolute w-3 h-3 top-0 right-0 "></div>
                         </div>
                             
-                       
-                            
-                        
                     </div>
                     
                     
@@ -83,7 +85,7 @@ const displayPosts = post =>{
                             <path id="Vector" d="M6.58334 6.58337L15.9167 6.58337M6.58334 11.25L13.5833 11.25M4.25 17.0834C3.32174 17.0834 2.4315 16.7146 1.77512 16.0582C1.11875 15.4019 0.75 14.5116 0.75 13.5834L0.75 4.25C0.75 3.32178 1.11875 2.43152 1.77512 1.77515C2.4315 1.11877 3.32174 0.75 4.25 0.75L18.25 0.75C19.1783 0.75 20.0685 1.11877 20.7249 1.77515C21.3812 2.43152 21.75 3.32178 21.75 4.25L21.75 13.5834C21.75 14.5116 21.3812 15.4019 20.7249 16.0582C20.0685 16.7146 19.1783 17.0834 18.25 17.0834L14.75 17.0834L11.25 20.5834L7.75 17.0834L4.25 17.0834Z" stroke="#12132D" stroke-opacity="0.600000" stroke-width="1.500000" stroke-linejoin="round"/>
                             </svg>
                             
-                            <p class="">${postInfo.comment_count}</p>
+                            <p class="text-text-color">${postInfo.comment_count}</p>
                             </div>
 
                             <div class="flex gap-4 items-center">
@@ -95,7 +97,7 @@ const displayPosts = post =>{
                                     <path id="Vector" d="M9.60009 9.3999C10.0377 9.83752 10.6312 10.0834 11.25 10.0834C11.8688 10.0834 12.4623 9.83752 12.8999 9.3999C13.3375 8.96228 13.5833 8.3689 13.5833 7.75C13.5833 7.1311 13.3375 6.53772 12.8999 6.1001C12.4623 5.66248 11.8688 5.41663 11.25 5.41663C10.6312 5.41663 10.0377 5.66248 9.60009 6.1001C9.1625 6.53772 8.91667 7.1311 8.91667 7.75C8.91667 8.3689 9.1625 8.96228 9.60009 9.3999ZM11.25 14.75C7.05 14.75 3.55 12.4166 0.75 7.75C3.55 3.08337 7.05 0.75 11.25 0.75C15.45 0.75 18.95 3.08337 21.75 7.75C18.95 12.4166 15.45 14.75 11.25 14.75Z" stroke="#12132D" stroke-opacity="0.600000" stroke-width="1.500000" stroke-linejoin="round"/>
                                     </svg>
 
-                                <p class="">${postInfo.view_count}</p>
+                                <p class="text-text-color">${postInfo.view_count}</p>
                             </div>
 
                             <div class="flex gap-4 items-center">
@@ -107,12 +109,12 @@ const displayPosts = post =>{
                                     <path id="Vector" d="M1.54926 15.2682C2.07694 16.5421 2.85036 17.6996 3.82538 18.6746C4.80039 19.6497 5.95791 20.4231 7.23183 20.9507C8.50574 21.4784 9.87112 21.75 11.25 21.75C12.6289 21.75 13.9943 21.4784 15.2682 20.9507C16.5421 20.4231 17.6996 19.6497 18.6746 18.6746C19.6496 17.6996 20.4231 16.5421 20.9507 15.2682C21.4784 13.9943 21.75 12.6289 21.75 11.25C21.75 9.87109 21.4784 8.50574 20.9507 7.23181C20.4231 5.95789 19.6496 4.80042 18.6746 3.82544C17.6996 2.85034 16.5421 2.0769 15.2682 1.54932C13.9943 1.02161 12.6289 0.75 11.25 0.75C9.87112 0.75 8.50574 1.02161 7.23183 1.54932C5.95791 2.0769 4.80039 2.85034 3.82538 3.82544C2.85036 4.80042 2.07694 5.95789 1.54926 7.23181C1.02159 8.50574 0.75 9.87109 0.75 11.25C0.75 12.6289 1.02159 13.9943 1.54926 15.2682ZM11.25 5.41663L11.25 11.25L7.16666 11.25" stroke="#12132D" stroke-opacity="0.600000" stroke-width="1.500000" stroke-linejoin="round" stroke-linecap="round"/>
                                     </svg>
 
-                                <p class="">${postInfo.posted_time}</p>
+                                <p class="text-text-color">${postInfo.posted_time}<span> Min</span></p>
 
                             </div>
 
                         </div>
-                        <div class="w-[10%]">
+                        <div class="w-[10%]" id="click">
                         <svg width="27.999817" height="28.000000" viewBox="0 0 27.9998 28" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                         <desc>
                                 Created with Pixso.
@@ -135,8 +137,47 @@ const displayPosts = post =>{
 
 
         `;
+        
+        const activeColorDiv = postCard.querySelector('#activeColor');
+        activeColorDiv.style.backgroundColor = active ? 'green' : 'red';
+
         postContainer.appendChild(postCard);
+
+
+        // click and append
+
+
+        const clickMessage = postCard.querySelector('#click');
+        clickMessage.addEventListener('click', () => {
+            
+            const appendDiv = document.createElement(`div`);
+            appendDiv.classList.add('bg-white','h-[70px]','w-[90%]','mb-2','rounded-xl','mx-auto','p-3');
+            appendDiv.innerHTML = `
+                <div class="flex justify-between pt-3">
+                    <p>${postInfo.title}</p>
+                    <div class="flex items-center gap-2">
+                          <svg width="22.500000" height="15.500000" viewBox="0 0 22.5 15.5" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                          <desc>
+                              Created with Pixso.
+                          </desc>
+                          <defs/>
+                          <path id="Vector" d="M9.6001 9.3999C10.0377 9.83752 10.6312 10.0834 11.25 10.0834C11.8688 10.0834 12.4623 9.83752 12.8999 9.3999C13.3375 8.96228 13.5833 8.3689 13.5833 7.75C13.5833 7.1311 13.3375 6.5376 12.8999 6.1001C12.4623 5.6626 11.8688 5.41675 11.25 5.41675C10.6312 5.41675 10.0377 5.6626 9.6001 6.1001C9.16248 6.5376 8.91669 7.1311 8.91669 7.75C8.91669 8.3689 9.16248 8.96228 9.6001 9.3999ZM11.25 14.75C7.04999 14.75 3.54999 12.4166 0.75 7.75C3.54999 3.08325 7.04999 0.75 11.25 0.75C15.45 0.75 18.95 3.08325 21.75 7.75C18.95 12.4166 15.45 14.75 11.25 14.75Z" stroke="#12132D" stroke-opacity="0.600000" stroke-width="1.500000" stroke-linejoin="round"/>
+                        </svg>
+
+                        <p>${postInfo.view_count}</p>
+                        </div>
+                    
+                </div>
+            `;
+            // Append the new div to postReadMark
+            postReadMark.appendChild(appendDiv);
+        });
+        
+        
     })
         
+}
+function markRead(element){
+  let seat=document.getElementById(element);
 }
 loadPost();
